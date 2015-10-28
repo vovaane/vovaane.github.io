2d3f3b9ef87ea0cd5e18d8423c906e905e9526bd
Vovaane <mihalev@lookatme.ru>
2015-10-28 20:18:58 +0400

    fff

--- js/custom.js
+++ js/custom.js
@@ -25,7 +25,7 @@ jQuery(function ($) { "use strict";
 		cursorborderradius: 0,
 		cursorwidth: "8px",
 		cursorfixedheight: 150,
-		cursorcolor: "#5F9EEC",
+		cursorcolor: "#FFFFFF",
 		zindex: 9999,
 		cursorborder: 0,
 	});
@@ -35,45 +35,10 @@ jQuery(function ($) { "use strict";
 	/*	Scroll Up / Back to top
 	/* ========================================================================= */
 	
-	$(window).scroll(function() {
-		if ($(window).scrollTop() > 400) {
-			$("#scrollUp").fadeIn(200);
-		} else {
-			$("#scrollUp").fadeOut(200);
-		}
-	});
-	
-	$('#scrollUp').click(function() {
-		$('html, body').stop().animate({
-			scrollTop : 0
-		}, 1500, 'easeInOutExpo');
-	});
-
 
 	/* ========================================================================= */
 	/*	Post image slider
 	/* ========================================================================= */
-	
-	$("#post-thumb, #gallery-post").owlCarousel({
-
-		navigation : true,
-		pagination : false,
-		slideSpeed : 800,
-		autoHeight : true,
-		paginationSpeed : 800,
-		singleItem:true,
-		navigationText : ["<i class='fa fa-angle-left fa-2x'></i>","<i class='fa fa-angle-right fa-2x'></i>"]
-
-	});
-	
-	$("#features").owlCarousel({
-		navigation : false,
-		pagination : true,
-		slideSpeed : 800,
-		singleItem : true,
-		transitionStyle : "fadeUp",
-		paginationSpeed : 800,
-	});
 
 
 	/* ========================================================================= */
@@ -165,11 +130,6 @@ jQuery(function ($) { "use strict";
 	/*	Twitter Feed
 	/* ========================================================================= */
 	
-	$(".tweet").twittie({
-        dateFormat: "%b. %d, %Y",
-        template: "{{tweet}}",
-        count: 1
-    });
 
 
 	/* ========================================================================= */
@@ -200,86 +160,7 @@ jQuery(function ($) { "use strict";
 	/* ========================================================================= */
 
 
-	$('#contact-submit').click(function (e) {
-
-		//stop the form from being submitted
-		e.preventDefault();
-
-		/* declare the variables, var error is the variable that we use on the end
-		to determine if there was an error or not */
-		var error = false;
-		var name = $('#name').val();
-		var email = $('#email').val();
-		var subject = $('#subject').val();
-		var message = $('#message').val();
-
-		/* in the next section we do the checking by using VARIABLE.length
-		where VARIABLE is the variable we are checking (like name, email),
-		length is a JavaScript function to get the number of characters.
-		And as you can see if the num of characters is 0 we set the error
-		variable to true and show the name_error div with the fadeIn effect. 
-		if it's not 0 then we fadeOut the div( that's if the div is shown and
-		the error is fixed it fadesOut. 
-		
-		The only difference from these checks is the email checking, we have
-		email.indexOf('@') which checks if there is @ in the email input field.
-		This JavaScript function will return -1 if no occurrence have been found.*/
-		if (name.length == 0) {
-			var error = true;
-			$('#name').css("border-color", "#D8000C");
-		} else {
-			$('#name').css("border-color", "#666");
-		}
-		if (email.length == 0 || email.indexOf('@') == '-1') {
-			var error = true;
-			$('#email').css("border-color", "#D8000C");
-		} else {
-			$('#email').css("border-color", "#666");
-		}
-		if (subject.length == 0) {
-			var error = true;
-			$('#subject').css("border-color", "#D8000C");
-		} else {
-			$('#subject').css("border-color", "#666");
-		}
-		if (message.length == 0) {
-			var error = true;
-			$('#message').css("border-color", "#D8000C");
-		} else {
-			$('#message').css("border-color", "#666");
-		}
-
-		//now when the validation is done we check if the error variable is false (no errors)
-		if (error == false) {
-			//disable the submit button to avoid spamming
-			//and change the button text to Sending...
-			$('#contact-submit').attr({
-				'disabled': 'false',
-				'value': 'Sending...'
-			});
-
-			/* using the jquery's post(ajax) function and a lifesaver
-			function serialize() which gets all the data from the form
-			we submit it to send_email.php */
-			$.post("sendmail.php", $("#contact-form").serialize(), function (result) {
-				//and after the ajax request ends we check the text returned
-				if (result == 'sent') {
-					//if the mail is sent remove the submit paragraph
-					$('#cf-submit').remove();
-					//and show the mail success div with fadeIn
-					$('#mail-success').fadeIn(500);
-				} else {
-					//show the mail failed div
-					$('#mail-fail').fadeIn(500);
-					//re enable the submit button by removing attribute disabled and change the text back to Send The Message
-					$('#contact-submit').removeAttr('disabled').attr('value', 'Send The Message');
-				}
-			});
-		}
-	});
-
-
-
+	
 	/* ========================================================================= */
 	/*	Google Map Customization
 	/* =========================================================================  */
@@ -590,23 +471,13 @@ $(function() {
 "use strict";
 
 function parallaxInit() {
-	$('#counter').parallax("50%", 0.3);
 	$('#team-skills').parallax("50%", 0.3);
-	$('#twitter-feed').parallax("50%", 0.3);
-	$('#testimonial').parallax("50%", 0.3);
 }
 
 $(window).bind("load", function () {
     parallaxInit()
 });
-$(document).ready(function(){
-  $('.bxslider').bxSlider();
-});
 
-$('.bxslider').bxSlider({
-  auto: true,
-  autoControls: true
-});
 
 $(document).ready(function(){
   $('.slider1').bxSlider({
