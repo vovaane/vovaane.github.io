d26ab438edf35d684025d827ff7f74b3bb0365c6
vovaane <mihalev@lookatme.ru>
2015-11-05 16:15:27 +0300

    new ccool

---
+++ js/custom.js
@@ -0,0 +1,499 @@
+                                
+
+jQuery(function ($) { "use strict";
+	
+	/* ========================================================================= */
+	/*	Page Preloader
+	/* ========================================================================= */
+	
+	window.onload = function () {
+		document.getElementById('loading-mask').style.display = 'none';
+	}
+
+	/* =========================================================================== */
+	/*	FitVids js
+	/* =========================================================================== */
+	
+	$(".media-wrapper").fitVids();
+
+	
+	/* ========================================================================= */
+	/*	Nice Scroll - Custom Scrollbar
+	/* ========================================================================= */
+
+	var nice = $("html").niceScroll({
+		cursorborderradius: 0,
+		cursorwidth: "8px",
+		cursorfixedheight: 150,
+		cursorcolor: "blue",
+		zindex: 9999,
+		cursorborder: 0,
+	});
+
+
+	/* ========================================================================= */
+	/*	Scroll Up / Back to top
+	/* ========================================================================= */
+	
+
+	/* ========================================================================= */
+	/*	Post image slider
+	/* ========================================================================= */
+
+
+	/* ========================================================================= */
+	/*	Menu item highlighting
+	/* ========================================================================= */
+
+
+	$("#navigation").sticky({
+		topSpacing : 0
+	});
+	
+	$('#nav').onePageNav({
+		currentClass: 'current',
+		changeHash: false,
+		scrollSpeed: 1500,
+		scrollThreshold: 0.5,
+		filter: '',
+		easing: 'easeInOutExpo'
+	});
+	
+
+
+	/* ========================================================================= */
+	/*	Fix Slider Height
+	/* ========================================================================= */	
+
+	var slideHeight = $(window).height();
+	
+	$('#slitSlider, .sl-slider, .sl-content-wrapper').css('height',slideHeight);
+
+	$(window).resize(function(){'use strict',
+		$('#slitSlider, .sl-slider, .sl-content-wrapper').css('height',slideHeight);
+	});
+	
+	
+
+	/* ========================================================================= */
+	/*	Timer count
+	/* ========================================================================= */
+
+	(function() {
+		var count = {
+		  initialized : false,
+		  initialize : function() {
+			if (this.initialized)
+			  return;
+			this.initialized = true;
+			this.build();
+		  },
+		  build : function() {
+			this.animations();
+		  },
+		  animations : function() {
+			// Count To
+			$(".cool [data-to]").each(function() {
+			  var $this = $(this);
+			  $this.appear(function() {
+				$this.countTo({});
+			  }, {
+				accX : 0,
+				accY : -150
+			  });
+			});
+		  },
+		};
+		count.initialize();
+	})();
+
+
+	/* ========================================================================= */
+	/*	Skills Chart
+	/* ========================================================================= */
+
+	$(".chart").appear(function () {
+		$(".chart").easyPieChart({
+			easing: "easeOutBounce",
+			barColor: "#6CB670",
+			size: "150",
+			lineWidth: 15,
+			animate: 2e3,
+			onStep: function (e, t, n) {
+				$(this.el).find(".percent").text(Math.round(n))
+			}
+		})
+	});
+	
+	
+	/* ========================================================================= */
+	/*	Twitter Feed
+	/* ========================================================================= */
+	
+
+
+	/* ========================================================================= */
+	/*	Portfolio Filtering Hook
+	/* =========================================================================  */
+
+	$('#og-grid').mixItUp(); // Portfolio filter
+
+	Grid.init(); //Portfolio Grid Expand
+	
+	
+	/* ========================================================================= */
+	/*	Testimonial Carousel
+	/* =========================================================================  */
+ 
+	//Init the carousel
+	$("#testimonials").owlCarousel({
+		slideSpeed: 500,
+		paginationSpeed: 500,
+		singleItem: true,
+		pagination : true,
+		transitionStyle : "backSlide"
+	});
+
+
+	/* ========================================================================= */
+	/*   Contact Form Validating
+	/* ========================================================================= */
+
+
+	
+	/* ========================================================================= */
+	/*	Google Map Customization
+	/* =========================================================================  */
+
+	function initialize() {
+
+		var myLatLng = new google.maps.LatLng(55.7899249, 37.7099948);
+		var myLatLng1 = new google.maps.LatLng(55.7933775, 37.6819173);
+
+		var roadAtlasStyles = [
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#e9e9e9"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dedede"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#333333"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    }
+]
+
+		var mapOptions = {
+			zoom: 14,
+			center: myLatLng1,
+			disableDefaultUI: true,
+			scrollwheel: false,
+			navigationControl: false,
+			mapTypeControl: false,
+			scaleControl: false,
+			draggable: false,
+			mapTypeControlOptions: {
+				mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'roadatlas']
+			}
+		};
+
+		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+
+		var marker = new google.maps.Marker({
+			position: myLatLng,
+			map: map,
+			icon: 'img/location-icon.png',
+			title: '',
+		});
+
+
+		google.maps.event.addListener(marker, 'click', function () {
+			infowindow.open(map, marker);
+		});
+
+		var styledMapOptions = {
+			name: 'US Road Atlas'
+		};
+
+		var usRoadMapType = new google.maps.StyledMapType(
+			roadAtlasStyles, styledMapOptions);
+
+		map.mapTypes.set('roadatlas', usRoadMapType);
+		map.setMapTypeId('roadatlas');
+	}
+
+	google.maps.event.addDomListener(window, "load", initialize);
+
+
+});
+
+/* ========================================================================= */
+/*	On scroll fade/bounce fffect
+/* ========================================================================= */
+
+	wow = new WOW({
+		animateClass: 'animated',
+		offset: 120
+	});
+	wow.init();
+	
+
+/* ========================================================================= */
+/*	Home page Slider
+/* ========================================================================= */
+
+$(function() {
+
+	var Page = (function() {
+
+		var $navArrows = $( '#nav-arrows' ),
+			$nav = $( '#nav-dots > span' ),
+			slitslider = $( '#slitSlider' ).slitslider( {
+			
+			    speed : 1600,
+			
+				onBeforeChange : function( slide, pos ) {
+
+					$nav.removeClass( 'nav-dot-current' );
+					$nav.eq( pos ).addClass( 'nav-dot-current' );
+
+				}
+			} ),
+
+			init = function() {
+				initEvents();				
+			},
+			initEvents = function() {
+				// add navigation events
+				$navArrows.children( ':last' ).on( 'click', function() {
+					slitslider.next();
+					return false;
+				} );
+
+				$navArrows.children( ':first' ).on( 'click', function() {					
+					slitslider.previous();
+					return false;
+				});
+
+				$nav.each( function( i ) {				
+					$( this ).on( 'click', function( event ) {						
+						var $dot = $( this );						
+						if( !slitslider.isActive() ) {
+							$nav.removeClass( 'nav-dot-current' );
+							$dot.addClass( 'nav-dot-current' );						
+						}
+						
+						slitslider.jump( i + 1 );
+						return false;
+					
+					});					
+				});
+			};
+			return { init : init };
+
+	})();
+
+	Page.init();
+
+});
+
+
+/* ========================================================================= */
+/*	Parallax Sections
+/* ========================================================================= */
+
+
+"use strict";
+
+
+
+$(document).ready(function(){
+  $('.slider1').bxSlider({
+    slideWidth: 400,
+    minSlides: 1,
+    maxSlides: 3,
+    slideMargin: 0
+  });
+});
+
+
+
+(function(){
+    $(window).scroll(function() {
+    
+        // check if window scroll for more than 430px. May vary
+        // as per the height of your main banner.
+        
+        if($(this).scrollTop() > 490) { 
+            $('.navbar').addClass('opaque'); // adding the opaque class
+        } else {
+            $('.navbar').removeClass('opaque'); // removing the opaque class
+        }
+    });
+  })();
+                            
