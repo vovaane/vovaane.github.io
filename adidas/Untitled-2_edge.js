4f20a40c83ac4e262be2c2ed30b775235c4cc1a7
vovaane <mihalev@lookatme.ru>
2016-03-03 17:11:48 +0300

    goo

---
+++ adidas/Untitled-2_edge.js
@@ -0,0 +1,62 @@
+/*jslint */
+/*global AdobeEdge: false, window: false, document: false, console:false, alert: false */
+(function (compId) {
+
+    "use strict";
+    var im='images/',
+        aud='media/',
+        vid='media/',
+        js='js/',
+        fonts = {
+        },
+        opts = {
+            'gAudioPreloadPreference': 'auto',
+            'gVideoPreloadPreference': 'auto'
+        },
+        resources = [
+        ],
+        scripts = [
+        ],
+        symbols = {
+            "stage": {
+                version: "6.0.0",
+                minimumCompatibleVersion: "5.0.0",
+                build: "6.0.0.400",
+                scaleToFit: "none",
+                centerStage: "none",
+                resizeInstances: false,
+                content: {
+                    dom: [
+                        {
+                            id: 'a102002_H264_8Mbit_1920x1080',
+                            type: 'video',
+                            tag: 'video',
+                            rect: ['-510px', '-101px', '1339px', '766px', 'auto', 'auto'],
+                            autoplay: 'autoplay',
+                            source: [vid+"a102002_H264_8Mbit_1920x1080.mp4"],
+                            preload: 'auto'
+                        }
+                    ],
+                    style: {
+                        '${Stage}': {
+                            isStage: true,
+                            rect: [undefined, undefined, '320px', '570px'],
+                            overflow: 'hidden',
+                            fill: ["rgba(255,255,255,1)"]
+                        }
+                    }
+                },
+                timeline: {
+                    duration: 0,
+                    autoPlay: true,
+                    data: [
+
+                    ]
+                }
+            }
+        };
+
+    AdobeEdge.registerCompositionDefn(compId, symbols, fonts, scripts, resources, opts);
+
+    if (!window.edge_authoring_mode) AdobeEdge.getComposition(compId).load("Untitled-2_edgeActions.js");
+})("EDGE-309737808");
